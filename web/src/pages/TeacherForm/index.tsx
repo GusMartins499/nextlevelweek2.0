5e8985b22f4617f0e25abdd326dbd2fa94a79732
gusmartins499 <atendimento9@cubosis.com.br>
2020-08-12 13:21:31 -0300

    new components

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,8 @@ import './styles.css';
 
 import Input from '../../components/Input';
 import warningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
   return (
@@ -17,30 +19,30 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
+
           <Input name="name" label="Nome completo" />
-
           <Input name="avatar" label="Avatar" />
-
           <Input name="whatsapp" label="whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" />
 
+          <Select name="subject" label="Matéria" />
           <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
 
-          <footer>
-            <p>
-              <img src={warningIcon} alt="Aviso importante" />
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
               Importante ! <br />
               Preencha todos os dados
             </p>
-            <button type="button">
-              Salvar cadastro
+          <button type="button">
+            Salvar cadastro
             </button>
-          </footer>
-        </fieldset>
+        </footer>
       </main>
     </div>
   )
